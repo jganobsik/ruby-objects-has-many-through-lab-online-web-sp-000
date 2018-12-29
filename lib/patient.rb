358cdb8f73d9d23b950dfc34fcee6df701fb78e9
Joe Ganobsik <jganobsik@gmail.com>
2018-12-29 23:05:09 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,3 +1,14 @@
 class Patient
   @@all = []
-  attr_accessor :name, 
+  attr_accessor :name, :doctors, :appointments
+  
+  def initialize(name)
+    @name = name
+    @doctors = []
+    @appointments = []
+    @@all << self
+  end
+  
+  def new_appointment (doctor, appointment)
+    Appointment.new(self, doctor, appointment)
+  end
